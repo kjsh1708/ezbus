aa1e9aa0e4b73732c318e649ca808cd94641c67e
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2022-03-15 00:24:31 +0800

    feat: minor adjustment to the database; mock data ready

--- database/Database.sql
+++ database/Database.sql
@@ -3,6 +3,7 @@ create database ezBusDB;
 
 use ezBusDB; 
 
+-- Table 1
 drop table if exists bus; 
 create table bus(
     bus_id int not null auto_increment,
@@ -14,6 +15,7 @@ create table bus(
     constraint bus_pk primary key (bus_id)
 );
 
+-- Table 2
 drop table if exists bus_stop; 
 create table bus_stop(
     bus_stop_id int not null auto_increment, 
@@ -23,12 +25,13 @@ create table bus_stop(
     constraint bus_stop_id primary key (bus_stop_id)
 ); 
 
+-- Table 3
 drop table if exists arrivals; 
 create table arrivals(
     bus_id int not null, 
     bus_stop_id int not null,
     date_time datetime not null, 
-    duration time, 
+    duration int, -- in seconds
     alight_number int, 
     onboard_number int, 
     
@@ -37,6 +40,7 @@ create table arrivals(
     constraint arrivals_fk2 foreign key (bus_stop_id) references bus_stop(bus_stop_id)
 ); 
 
+-- Table 4 
 drop table if exists bus_route; 
 create table bus_route(
     route_id int not null auto_increment, 
@@ -46,6 +50,7 @@ create table bus_route(
     constraint bus_route_pk primary key (route_id)
 );
 
+-- Table 5
 drop table if exists route_stop;
 create table route_stop(
     route_id int not null,   
@@ -57,18 +62,19 @@ create table route_stop(
     constraint route_stop_fk2 foreign key (bus_stop_id) references bus_stop(bus_stop_id)
 );
 
-
+-- Table 6
 drop table if exists schedule;  
 create table schedule(
     bus_id int not null, 
     route_id int not null, 
-    depart_date_time datetime not null,
+    depart_time time not null,
 
-    constraint schedule_pk primary key (bus_id, route_id, depart_date_time), 
+    constraint schedule_pk primary key (bus_id, route_id, depart_time), 
     constraint schedule_fk1 foreign key (route_id) references bus_route(route_id), 
     constraint schedule_fk2 foreign key (bus_id) references bus(bus_id)
 );
 
+--  Table 7 
 create table crowd_level(
     bus_stop_id int not null, 
     date_time datetime not null, 
